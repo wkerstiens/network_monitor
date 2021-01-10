f2a5a20f523578db2d82db82e2db675f9a200ba7
Tony Kerstiens <tony.kerstiens@gmail.com>
2021-01-10 16:08:45 -0700

    Initial Checkin

---
+++ source/index.ts
@@ -0,0 +1,11 @@
+
+(async () : Promise<number> => {
+    let errorCode = 0;
+    const startTime = new Date().toISOString();
+    try {
+    } catch (exception) {
+
+    } finally {
+        return errorCode;
+    }
+})();
