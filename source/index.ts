498ce308e37da52661a073c19a17c8aa6681998f
Tony Kerstiens <tony.kerstiens@gmail.com>
2021-01-10 21:56:34 -0700

    Added more verbiage

--- source/index.ts
+++ source/index.ts
@@ -14,9 +14,10 @@ import {sleep} from "./util/sleep.util";
     let currentState : string | null = null;
 
     try {
+        console.clear();
         while(true) {
             const start_of_ping = moment();
-            const res = await ping.promise.probe('www.lttrust.com');
+            const res = await ping.promise.probe('www.lttrust.com', { timeout: 0, extra: ['-t', '0'] });
             const end_of_ping = moment();
 
             if(res.alive === true) {
@@ -42,16 +43,24 @@ import {sleep} from "./util/sleep.util";
             }
             console.clear();
             console.log(`
-                Start of logging:        ${startOfLogging?.format('MM-DD-YYYY HH:mm:ss')}
-            
-                Start of Last Uptime:    ${startOfLastUpTime?.format('MM-DD-YYYY HH:mm:ss')}
-                End of Last Uptime:      ${endOfLastUpTime?.format('MM-DD-YYYY HH:mm:ss')}
-                
-                Start of Last Downtime:  ${startOfLastDownTime?.format('MM-DD-YYYY HH:mm:ss')}
-                End of Last Downtime:    ${endOfLastDownTime?.format('MM-DD-YYYY HH:mm:ss')}
-                
-                Total Time Network Has Been Available:    ${totalUptime} seconds
-                Total Time Network Has Been Unavailable:  ${totalDowntime} seconds
+    Start of logging:        ${startOfLogging?.format('MM-DD-YYYY HH:mm:ss')}
+
+    Start of Last Uptime:    ${startOfLastUpTime?.format('MM-DD-YYYY HH:mm:ss')}    Start of Last Downtime:  ${startOfLastDownTime?.format('MM-DD-YYYY HH:mm:ss')}
+    End of Last Uptime:      ${endOfLastUpTime?.format('MM-DD-YYYY HH:mm:ss')}    End of Last Downtime:    ${endOfLastDownTime?.format('MM-DD-YYYY HH:mm:ss')}
+    
+    Total Time Available:    ${totalUptime.toString().padStart(10)} seconds    Total Time Unavailable:  ${totalDowntime.toString().padStart(10)} seconds
+    Total Time Available:    ${(totalUptime / 60).toFixed(2).padStart(10)} minutes    Total Time Unavailable:  ${(totalDowntime / 60).toFixed(2).padStart(10)} minutes
+    Total Time Available:    ${(totalUptime / 60 / 60).toFixed(2).padStart(10)} hours      Total Time Unavailable:  ${(totalDowntime / 60 / 60).toFixed(2).padStart(10)} hours
+    Total Time Available:    ${(totalUptime / 60 / 60 / 24).toFixed(2).padStart(10)} days       Total Time Unavailable:  ${(totalDowntime / 60 / 60 / 24).toFixed(2).padStart(10)} days
+
+
+    
+    
+    
+    
+    
+    Network is currently ${currentState}
+    Network has been up ${(totalUptime/(totalUptime+totalDowntime) * 100).toFixed(2)}% of the time since this session began.
             `);
             await sleep(2);
         }
